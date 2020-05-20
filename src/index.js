6819360a848310f425f397d8a5b75e8e209bfa8b
bcwan <bcwan8@gmail.com>
2020-05-20 14:56:38 -0700

    Update block name in index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const game = new Game();
     
     // test code
-    let block1 = new Block();
+    let testBlock = new Block();
     let grid = document.querySelector("#grid");
-    grid.appendChild(block1.block);
+    grid.appendChild(testBlock);
 })
