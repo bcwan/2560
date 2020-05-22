61f86808055314fe3d84f86ea564d38b32a5b68b
bcwan <bcwan8@gmail.com>
2020-05-22 10:27:54 -0700

    Update grid div to grid-container name

--- src/game.js
+++ src/game.js
@@ -5,11 +5,12 @@ class Game {
   constructor() {
     this.board = new Board();
     this.setUpBoard();
+    this.setUpBlockContainer();
   }
 
   setUpBoard() {  
     const grid = document.createElement("div");
-    grid.id = "grid";
+    grid.id = "grid-container";
 
     for (let i = 0; i < this.board.grid.length; i++) {
       let row = document.createElement("div");
@@ -23,12 +24,14 @@ class Game {
       
       grid.appendChild(row);
     }
-    
+
     document.getElementById("board").appendChild(grid);
   }
 
   setUpBlockContainer() {
-
+    const blockContainer = document.createElement('div');
+    blockContainer.id = "block-container";
+    document.getElementById("board").appendChild(blockContainer);
   }
 
   
