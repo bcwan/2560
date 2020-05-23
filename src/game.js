012af052c936c8dd774ba37a15ff84d557b6bf76
bcwan <bcwan8@gmail.com>
2020-05-23 15:49:27 -0700

    Create hash table to match grid index to class position

--- src/game.js
+++ src/game.js
@@ -1,6 +1,34 @@
 let Board = require("./board"); 
 let Block = require('./block');
 
+const BOARD_TO_GRID = {
+  "0,0": "grid-0-0",
+  "0,1": "grid-0-1",
+  "0,2": "grid-0-2",
+  "0,3": "grid-0-3",
+  "0,4": "grid-0-4",
+  "1,0": "grid-1-0",
+  "1,1": "grid-1-1",
+  "1,2": "grid-1-2",
+  "1,3": "grid-1-3",
+  "1,4": "grid-1-4",
+  "2,0": "grid-2-0",
+  "2,1": "grid-2-1",
+  "2,2": "grid-2-2",
+  "2,3": "grid-2-3",
+  "2,4": "grid-2-4",
+  "3,0": "grid-3-0",
+  "3,1": "grid-3-1",
+  "3,2": "grid-3-2",
+  "3,3": "grid-3-3",
+  "3,4": "grid-3-4",
+  "4,0": "grid-4-0",
+  "4,1": "grid-4-1",
+  "4,2": "grid-4-2",
+  "4,3": "grid-4-3",
+  "4,4": "grid-4-4"
+};
+
 class Game {
   constructor() {
     this.board = new Board();
