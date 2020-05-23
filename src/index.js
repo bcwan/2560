fa7548064c5b83ab84d46314302afc60a045684f
bcwan <bcwan8@gmail.com>
2020-05-23 13:14:19 -0700

    Add block piece to board

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ document.addEventListener("DOMContentLoaded", function () {
     
     // test code
     let testBlock = new Block();
+    game.board.grid[0][0] = testBlock;
     let blockContainer = document.querySelector("#block-container");
     blockContainer.appendChild(testBlock.block);
 
@@ -16,18 +17,22 @@ document.addEventListener("DOMContentLoaded", function () {
             // left
             case 37: 
                 testBlock.updateClassPosition(2, 0);
+                console.log(game.board.grid);
                 break;
             // right
             case 39:  
                 testBlock.updateClassPosition(2, 4);
+                console.log(game.board.grid);
                 break;
             // up
             case 38:  
                 testBlock.updateClassPosition(1, 0);
+                console.log(game.board.grid);
                 break;
             // down
             case 40: 
                 testBlock.updateClassPosition(1, 4);
+                console.log(game.board.grid);
                 break;
         }
     }
