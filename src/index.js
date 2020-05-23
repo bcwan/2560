d29624aec3f9768cbab239839843342526ce4962
bcwan <bcwan8@gmail.com>
2020-05-23 16:53:26 -0700

    Remove comments in index.js

--- src/index.js
+++ src/index.js
@@ -11,33 +11,20 @@ document.addEventListener("DOMContentLoaded", function () {
     let blockContainer = document.querySelector("#block-container");
     blockContainer.appendChild(testBlock.block);
     
-    console.log(game.board);
     function onKeyPressed (event) {
 
         switch (event.which) {
-            // left
             case 37: 
-                //testBlock.updateClassPosition(2, 0);
                 game.updateBoardMovement("left");
-                console.log(game.board.grid);
                 break;
-            // right
             case 39:  
-                //testBlock.updateClassPosition(2, 4);
                 game.updateBoardMovement("right");
-                console.log(game.board.grid);
                 break;
-            // up
             case 38:  
-                //testBlock.updateClassPosition(1, 0);
                 game.updateBoardMovement("up");
-                console.log(game.board.grid);
                 break;
-            // down
             case 40: 
-                //testBlock.updateClassPosition(1, 4);
                 game.updateBoardMovement("down");
-                console.log(game.board.grid);
                 break;
         }
     }
