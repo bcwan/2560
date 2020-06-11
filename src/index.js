b5ea4255876ceaacee528f8ff965dbb0d3fdab1b
bcwan <bcwan8@gmail.com>
2020-06-10 17:10:00 -0700

    Prevent block movement if game is over

--- src/index.js
+++ src/index.js
@@ -6,28 +6,30 @@ document.addEventListener("DOMContentLoaded", function () {
     let game = new Game();
 
     function onKeyPressed (event) {
-        
-        switch (event.which) {
-            case 37: 
-                game.updateBoardMovementLeftUp("left");
-                game.addNewBlock();
-                console.log(game.board.grid);
-                break;
-            case 39:  
-                game.updateBoardMovementRightDown("right");
-                game.addNewBlock();
-                console.log(game.board.grid);
-                break;
-            case 38:  
-                game.updateBoardMovementLeftUp("up");
-                game.addNewBlock();
-                console.log(game.board.grid);
-                break;
-            case 40: 
-                game.updateBoardMovementRightDown("down");
-                game.addNewBlock();
-                console.log(game.board.grid);
-                break;
+        let gameOverCovering = document.getElementById('game-over-cover');
+        if (!gameOverCovering) {
+            switch (event.which) {
+                case 37: 
+                    game.updateBoardMovementLeftUp("left");
+                    game.addNewBlock();
+                    console.log(game.board.grid);
+                    break;
+                case 39:  
+                    game.updateBoardMovementRightDown("right");
+                    game.addNewBlock();
+                    console.log(game.board.grid);
+                    break;
+                case 38:  
+                    game.updateBoardMovementLeftUp("up");
+                    game.addNewBlock();
+                    console.log(game.board.grid);
+                    break;
+                case 40: 
+                    game.updateBoardMovementRightDown("down");
+                    game.addNewBlock();
+                    console.log(game.board.grid);
+                    break;
+            }
         }
     }
 
